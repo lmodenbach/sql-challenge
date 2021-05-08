52c106e2141d1ce974cea70e01b53fad55385184
lmodenbach <lmodenbach@gmail.com>
2021-05-08 14:51:14 -0700

    fixed ERD
    beginning queries

--- EmployeeSQL/EmployeeSQLqueries.sql
+++ EmployeeSQL/EmployeeSQLqueries.sql
@@ -1,4 +1,4 @@
---queries
+--Queries
 
 --List the following details of each employee: employee number, last name, first name, sex, and salary
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
@@ -12,7 +12,8 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
---List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name
+--List the manager of each department with the following information: department number, department name, 
+--the manager's employee number, last name, first name
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name  
 FROM dept_managers dm
 INNER JOIN departments d
@@ -20,12 +21,29 @@ ON dm.dept_no = d.dept_no
 INNER JOIN employees e
 ON dm.emp_no = e.emp_no;
 
---List the department of each employee with the following information: employee number, last name, first name, and department name
+--List the department of each employee with the following information: employee number, last name, first name, 
+--and department name
+SELECT d.department, e.emp_no, e.last_name, e.first_name
+FROM employees e
+INNER JOIN dept_emp de
+ON e.emp_no = de.emp_no
+INNER JOIN departments d
+ON de.dept_no = d.dept_no;
+
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B
 
---List all employees in the Sales department, including their employee number, last name, first name, and department name
 
---List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department 
+--name
+
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, 
+--and department name
+
+
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+
