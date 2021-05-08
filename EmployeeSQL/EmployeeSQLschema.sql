2ef8f88d5dce68a708938d0b1dea32f540fdd208
lmodenbach <lmodenbach@gmail.com>
2021-05-08 14:26:37 -0700

    notes added to schema, saves to correct locations

--- EmployeeSQL/EmployeeSQLschema.sql
+++ EmployeeSQL/EmployeeSQLschema.sql
@@ -1,11 +1,17 @@
---Order of operations based on dependencies demonstrated in the ERDs:
---titles, employees, departments, department managers, department employees, salaries
+--Order of operations based on dependencies demonstrated in the ERD:
+--NOTE: some fine tuned reordering would have worked, for instance departments could 
+--      come first just as the independant titles can 
+--NOTE: thanks to QuickDBD!
 
+
+--titles: independant, title_id is only/primary key
 CREATE TABLE titles (
 	title_id VARCHAR PRIMARY KEY,
 	titles VARCHAR
 );
 
+--employees: emp_title_id is a foreign key depending on titles.title_id,
+--           emp_no is the primary key used as foreign key in other tables
 CREATE TABLE employees (
 	emp_no INT PRIMARY KEY,
 	emp_title_id VARCHAR,
@@ -17,11 +23,16 @@ CREATE TABLE employees (
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
+--departments: independent, provides dept_no as primary key
 CREATE TABLE departments (
 	dept_no VARCHAR PRIMARY KEY,
 	dept_name VARCHAR
 );
 
+--dept_managers: foreign key dept_no depends on departments.dept_no, foreign
+--               key emp_no depends on employees.emp_no, primary key cannot be 
+--               a foreign key (must be unique) so we do a composite key of both 
+--               data fields
 CREATE TABLE dept_managers (
 	dept_no VARCHAR,
 	emp_no INT, 
@@ -30,6 +41,9 @@ CREATE TABLE dept_managers (
 	PRIMARY KEY (dept_no, emp_no)
 );
 
+--dept_emp: foreign key dept_no depends on departments.dept_no, foreign key emp_no
+--          depends on employees.emp_no, again primary key must be unique so we do
+--          a composite key
 CREATE TABLE dept_emp (
 	emp_no INT,
 	dept_no VARCHAR, 
@@ -38,6 +52,8 @@ CREATE TABLE dept_emp (
 	PRIMARY KEY (emp_no, dept_no)
 );
 
+--salaries: foreign key emp_no depends on employees.emp_no, salaries has no appropriate 
+--          primary key as both salary values and emp_no values can appear multiple times
 CREATE TABLE salaries (
 	emp_no INT,
 	salary INT,
