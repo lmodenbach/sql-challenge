4f47d42659d828d677ec3b8152a7d58a175fd387
lmodenbach <lmodenbach@gmail.com>
2021-05-10 15:19:41 -0700

    got 6th query

--- EmployeeSQL/EmployeeSQLqueries.sql
+++ EmployeeSQL/EmployeeSQLqueries.sql
@@ -10,7 +10,8 @@ ORDER BY e.emp_no;
 --List first name, last name, and hire date for employees who were hired in 1986
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+ORDER BY hire_date;
 
 --List the manager of each department with the following information: department number, department name, 
 --the manager's employee number, last name, first name
@@ -19,7 +20,8 @@ FROM dept_managers dm
 INNER JOIN departments d
 ON dm.dept_no = d.dept_no
 INNER JOIN employees e
-ON dm.emp_no = e.emp_no;
+ON dm.emp_no = e.emp_no
+ORDER BY dm.emp_no;
 
 --List the department of each employee with the following information: employee number, last name, first name, 
 --and department name
@@ -33,12 +35,18 @@ ORDER BY dept_name;
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' 
-AND last_name LIKE 'B%';
+AND last_name LIKE 'B%'
+ORDER BY last_name;
 
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department 
 --name
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON e.emp_no = de.emp_no
+INNER JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+ORDER BY emp_no;
 
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, 
